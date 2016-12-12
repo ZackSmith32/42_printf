7dee50de72f514a547e2dec668a6bd4dc02d8314
Zachary Smith <z.smith32@gmail.com>
2016-12-11 23:03:14 -0800

    updating the %p conversoin

--- srcs/funx_pi.c
+++ srcs/funx_pi.c
@@ -6,7 +6,7 @@
 /*   By: zsmith <zsmith@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/13 12:32:18 by zsmith            #+#    #+#             */
-/*   Updated: 2016/12/11 22:19:35 by zsmith           ###   ########.fr       */
+/*   Updated: 2016/12/11 23:01:49 by zsmith           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,11 +41,11 @@ static void p_width(conv_obj *obj)
 	char	*holder;
 	int		diff;
 
-	diff = obj->width - 2 - ft_strlen(obj->str);
-	if (diff > 0)
+	diff = obj->width - ft_strlen(obj->str);
+	if (diff > 2)
 	{
-		new_str = (char *)ft_memalloc(obj->width + 1 - 2);
-		ft_memset(new_str, obj->zero == 1 ? 48 : 32, (size_t)(obj->width) - 2);
+		new_str = (char *)ft_memalloc(obj->width + 1);
+		ft_memset(new_str, obj->zero == 1 ? 48 : 32, (size_t)(obj->width));
 		if (!obj->minus)
 			holder = new_str + diff;
 		else
@@ -59,10 +59,30 @@ static void p_width(conv_obj *obj)
 	}
 }
 
+void		p_prefix(conv_obj *obj)
+{
+	int		i;
+
+	i = 0;
+	while (obj->str[i] != 0)
+		i++;
+	if (obj->zero || obj->prec != -1)
+	{
+
+		obj->str[0] = '0';
+		obj->str[1] = 'x';
+	}
+	else
+	{
+		obj->str[i - 3] = '0';
+		obj->str[i - 2] = 'x';
+	}
+
+}
+
 void		p_func(conv_obj *obj, va_list args)
 {
 	void	*ptr;
-	char	*ret;
 
 	free(obj->str);
 	ptr = va_arg(args, void *);
@@ -72,11 +92,13 @@ void		p_func(conv_obj *obj, va_list args)
 	obj->str = ft_itoa_base((unsigned long long)ptr, 16);
 	p_precision(obj);
 	p_width(obj);
-	ret = (char *)ft_memalloc(ft_strlen(obj->str) + 3);
-	ft_strcpy(ret, "0x\0");
-	ft_strcat(ret, obj->str);
-	free(obj->str);
-	obj->str = ret;
+	printf("after width:>%s<\n", obj->str);
+	p_prefix(obj);
+	// ret = (char *)ft_memalloc(ft_strlen(obj->str) + 3);
+	// ft_strcpy(ret, "0x\0");
+	// ft_strcat(ret, obj->str);
+	// free(obj->str);
+	// obj->str = ret;
 }
 
 void	i_func(conv_obj *obj, va_list args)
